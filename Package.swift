22afbc426e317bb521a6cf0a3bd3015b174a60ab
Pedro Ferreira <pedro.ferreira@vision-box.com>
2025-05-23 08:38:17 +0100

    Add JWTDecode as binary

--- Package.swift
+++ Package.swift
@@ -12,12 +12,10 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "VBUtils",
-            type: .dynamic,
             targets: ["VBUtilsWrapper"]),
     ],
     dependencies: [
         .package(url: "https://github.com/vbmobile/matomo-sdk-ios", .exact("7.7.0")),
-        .package(url: "https://github.com/vbmobile/JWTDecode.swift", .exact("2.6.3")),
     ],
 
     targets: [
@@ -28,12 +26,17 @@ let package = Package(
             url: "https://vbmobileidstorage.blob.core.windows.net/ios/MobileIdSDKiOS/VBUtils/VBUtils-2.0.0.zip",
             checksum: "204a74aba77547ba71ea4b4a408e4048c9b5c8362d399ae17e35803b5f0599d4"
         ),
+        .binaryTarget(
+            name: "JWTDecode",
+            url: "https://vbmobileidstorage.blob.core.windows.net/ios/MobileIdSDKiOS/JWTDecode/JWTDecode-2.6.3.zip",
+            checksum: "5642db1982cfd167214a6e1c579fa089bf39f8dda63afefbb47f2fcb303b8ce2"
+        ),
         .target(
             name: "VBUtilsWrapper",
             dependencies: [
                 .target(name: "VBUtils"),
+                .target(name: "JWTDecode"),
                 .product(name: "MatomoTracker", package: "matomo-sdk-ios"),
-                .product(name: "JWTDecode", package: "JWTDecode.swift")
             ],
             path: "Sources",
             sources: ["dummy.swift"]
