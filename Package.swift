7b8034b2fd9bb6a522c35368bfccf41c1053a215
Tiago Silva <tiago.silva@vision-box.com>
2026-06-23 20:20:10 +0100

    TRAVELERID-12954: rename to AMAUtils 3.0.0

--- Package.swift
+++ Package.swift
@@ -1,40 +1,22 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
-    name: "VBUtils",
+    name: "AMAUtils",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "VBUtils",
-            targets: ["VBUtilsWrapper"]),
+            name: "AMAUtils",
+            targets: ["AMAUtils"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/vbmobile/matomo-sdk-ios", .exact("7.7.0")),
-    ],
-
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
-            name: "VBUtils",
-            url: "https://vbmobileidstorage.blob.core.windows.net/ios/MobileIdSDKiOS/VBUtils/VBUtils-2.0.2.zip",
-            checksum: "50ecc5477b816a1cad58f757f5392a5d08a571b48078ca5669930ec5aa4983de"
-        ),
-        .target(
-            name: "VBUtilsWrapper",
-            dependencies: [
-                .target(name: "VBUtils"),
-                .product(name: "MatomoTracker", package: "matomo-sdk-ios"),
-            ],
-            path: "Sources",
-            sources: ["dummy.swift"]
+            name: "AMAUtils",
+            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMAUtils/AMAUtils-3.0.0.zip",
+            checksum: "57d6f0e44fe7224ec33c32c509e90756020e1928d0c039ddf9058c71cc8e61f7"
         )
     ],
-    swiftLanguageVersions: [.v5]
+    swiftLanguageModes: [.v5]
 )
