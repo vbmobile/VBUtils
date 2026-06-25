c64d450e56e5bd6a2309ce7d90ce26295358df1a
Tiago Silva <tiago.silva@vision-box.com>
2026-06-25 13:16:38 +0100

    TRAVELERID-14915: restore AMAUtils wrapper target to provide MatomoTracker transitively

--- Package.swift
+++ Package.swift
@@ -9,13 +9,25 @@ let package = Package(
     products: [
         .library(
             name: "AMAUtils",
-            targets: ["AMAUtils"]),
+            targets: ["AMAUtilsWrapper"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/matomo-org/matomo-sdk-ios.git", exact: "7.7.0"),
     ],
     targets: [
         .binaryTarget(
             name: "AMAUtils",
             url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMAUtils/AMAUtils-3.0.1.zip",
             checksum: "60e76d8cedd4260c689cd7028a05a6da7cfb4af5836cb11e67769ce1f2e59e16"
+        ),
+        .target(
+            name: "AMAUtilsWrapper",
+            dependencies: [
+                .target(name: "AMAUtils"),
+                .product(name: "MatomoTracker", package: "matomo-sdk-ios"),
+            ],
+            path: "Sources",
+            sources: ["dummy.swift"]
         )
     ],
     swiftLanguageModes: [.v5]
